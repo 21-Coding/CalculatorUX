0abe30d6145023590b95fdc5c20646b53beaabb5
Judah Barton and Liam Kenna <liam@>
2019-10-23 13:57:56 -0700

    Add script connections to other operator types to calculator app

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,60 @@
+
+var add = function(pickle, number2) {
+	return pickle + number2;
+};
+
+var subtract = function(number1, number2) {
+	return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+	return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+	return number1 / number2;
+};
+
+var whatOpp = function(o) {
+	if (o === "+") {
+		return add;
+	} else if (o === "-") {
+		return subtract;
+	} else if (o === "x") {
+		return multiply;
+	} else if (o === "÷") {
+		return divide;
+	}
+}
+
+var activeNumber = 1;
+
+var toggleActiveNumber = function() {
+	if (activeNumber === 1) {
+		activeNumber = 2;
+	} else {
+		activeNumber = 1;
+	}
+};
+
 $(document).ready(function(){
 
-	var add = function(pickle, number2) {
-		return pickle + number2;
-	}
+	$(".clickable-number").click(function(event){
+		var pressedButton = $(event.target).text();
+		$(".active-num" + activeNumber).text(pressedButton);
+		toggleActiveNumber();
+	});
 
+	$(".clickable-opp").click(function(event){
+		var pressedButton = $(event.target).text();
+		$(".active-opperator").text(pressedButton);
+	});
 
-
-	console.log(add(1,6));
+	$(".clickable-equal").click(function(){
+		var num1 = parseInt($(".active-num1").text());
+		var num2 = parseInt($(".active-num2").text());
+		var oppFunction = whatOpp($(".active-opperator").text())
+		$(".answer").text(oppFunction(num1, num2))
+	});
 
 });
